a23f9ee28c88fe9f045070a89c284824892a663b
Muhammad  Sholehhudin <msholehhudin.ms@gmail.com>
2026-04-07 22:58:56 +0700

    feat: implement task repository

---
+++ src/repositories/task.respository.ts
@@ -0,0 +1,33 @@
+import prisma from '../config/prisma'
+import { Prisma, Task } from '@prisma/client'
+
+export class TaskRepository {
+  async create(data: Prisma.TaskCreateInput): Promise<Task> {
+    return prisma.task.create({ data })
+  }
+
+  async findAll(): Promise<Task[]> {
+    return prisma.task.findMany({
+      orderBy: { createdAt: 'desc' },
+    })
+  }
+
+  async findById(id: string): Promise<Task | null> {
+    return prisma.task.findUnique({
+      where: { id },
+    })
+  }
+
+  async update(id: string, data: Prisma.TaskUpdateInput): Promise<Task> {
+    return prisma.task.update({
+      where: { id },
+      data,
+    })
+  }
+
+  async delete(id: string): Promise<Task> {
+    return prisma.task.delete({
+      where: { id },
+    })
+  }
+}
