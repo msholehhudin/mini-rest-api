1d13dfac0f606d17dd8e30ba441bbb00a3952c84
Muhammad  Sholehhudin <msholehhudin.ms@gmail.com>
2026-04-07 23:43:09 +0700

    feat: implement app.ts and add cors package

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import cors from 'cors'
+import taskRoutes from './routes/task.route'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api', taskRoutes)
+
+export default app
